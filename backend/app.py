00660d7425f402b37e88d1946653040fe96445a0
Soumit Das <its.soumit.das@gmail.com>
2023-06-07 02:36:49 +0530

    Major UI changes, code changes to app.py

--- backend/app.py
+++ backend/app.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 app.secret_key = os.urandom(24)
 app.config['GOOGLE_CLIENT_ID'] = '960270714715-scg1n3nvl4734ld5mog1338la72kad75.apps.googleusercontent.com'
 app.config['GOOGLE_CLIENT_SECRET'] = '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'
+# app.config['GOOGLE_REDIRECT_URI'] = 'http://chrome.soumit.in:5000/callback'
 app.config['GOOGLE_REDIRECT_URI'] = 'http://127.0.0.1:5000/callback'
 app.config['GOOGLE_SCOPES'] = [
     'https://www.googleapis.com/auth/spreadsheets',
@@ -31,8 +32,10 @@ def login():
         file.save(os.path.join('uploads', file.filename))
 
         # read csv to list
-        df = pd.read_csv(os.path.join('uploads', file.filename), header=None)
-        os.remove(os.path.join('uploads', file.filename))
+        df = pd.read_csv(os.path.join('uploads', file.filename), on_bad_lines='skip', header=None)
+
+        # save csv to session
+        session['file'] = file.filename
 
     # csv from url
     else:
@@ -40,13 +43,15 @@ def login():
         url = request.form['url']
 
         # read csv to list
-        df = pd.read_csv(url, header=None)
+        df = pd.read_csv(url, on_bad_lines='skip', header=None)
+
+        # save csv to local storage
+        df.to_csv(f'uploads/{request.form["name"]}.csv', index=False)
+
+        # save csv to session
+        session['file'] = f'{request.form["name"]}.csv'
 
     name = request.form['name']
-    df = df.fillna('')
-    df_list = df.values.tolist()
-    print(df_list)
-    session['df_list'] = df_list
     session['name'] = name
 
     # start login process    
@@ -74,19 +79,7 @@ def callback():
     if not google_flow.credentials:
         return 'Failed to retrieve access token.'
 
-    # session['credentials'] = google_flow.credentials.to_json()
-    # return redirect('/create_spreadsheet')
     credentials = google_flow.credentials
-    session['credentials'] = credentials_to_dict(credentials)
-    return redirect('/create_spreadsheet')
-
-
-@app.route('/create_spreadsheet')
-def create_spreadsheet():
-    if 'credentials' not in session:
-        return redirect('/login')
-
-    credentials = google.oauth2.credentials.Credentials.from_authorized_user_info(session['credentials'])
     service = build('sheets', 'v4', credentials=credentials)
 
     spreadsheet_body = {
@@ -97,13 +90,17 @@ def create_spreadsheet():
     spreadsheet = service.spreadsheets().create(body=spreadsheet_body).execute()
     spreadsheet_id = spreadsheet['spreadsheetId']
 
-    df = pd.DataFrame(session.get('df_list'))
+    csv = session.get('file')
+    df = pd.read_csv(f'uploads/{csv}', on_bad_lines='skip', header=None)
+    df = df.fillna('')
     
     # Convert DataFrame to list of lists
     values = df.values.tolist()
 
+    os.remove(os.path.join('uploads', csv))
+
     # Prepare the update request
-    request = service.spreadsheets().values().update(
+    requestt = service.spreadsheets().values().update(
         spreadsheetId=spreadsheet_id,
         range='Sheet1!A1',
         valueInputOption='RAW',
@@ -113,14 +110,16 @@ def create_spreadsheet():
     )
 
     # Execute the update request
-    create = request.execute()
+    create = requestt.execute()
 
     response = f"""
     Your CSV is now in Google Sheets!
     <br>
     <a href="https://docs.google.com/spreadsheets/d/spreadsheet_id" target="_blank">https://docs.google.com/spreadsheets/d/spreadsheet_id</a>
     """
-    return render_template_string(response.replace("spreadsheet_id", spreadsheet_id))
+    # return render_template_string(response.replace("spreadsheet_id", spreadsheet_id))
+    return render_template('success.html', spreadsheet_id=spreadsheet_id)
+
 
 def credentials_to_dict(credentials):
     return {
@@ -134,4 +133,5 @@ def credentials_to_dict(credentials):
 
 if __name__ == '__main__':
     os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+    # app.run(host="0.0.0.0")
     app.run()
